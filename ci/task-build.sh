ffbd45ee06f26fb6623fbb7661fa46c3406a891b
David Jiménez <david.jimenez@solera.com>
2019-03-03 00:21:48 +0100

    Fix!

--- ci/task-build.sh
+++ ci/task-build.sh
@@ -2,7 +2,8 @@
 
 # move node_modules input into code folder
 echo 'Moving node_modules to output folder...'
-mv ../node_modules ./node_modules
+mkdir node_modules
+mv ../node_modules/* ./node_modules
 
 # build application
 echo 'Building application...'
