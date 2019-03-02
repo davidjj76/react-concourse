b1ae7ab5be8878199b8594742f3ba62dd85e1044
David Jiménez <david.jimenez@solera.com>
2019-03-03 00:25:20 +0100

    Fix!

--- ci/task-build.sh
+++ ci/task-build.sh
@@ -2,8 +2,9 @@
 
 # move node_modules input into code folder
 echo 'Moving node_modules to output folder...'
+ls ../node_modules
 mkdir node_modules
-mv ../node_modules/* ./node_modules
+cp ../node_modules/* ./node_modules
 
 # build application
 echo 'Building application...'
--- ci/task-install.sh
+++ ci/task-install.sh
@@ -6,6 +6,4 @@ npm install
 
 # move node_modules to output
 echo 'Moving node_modules to output folder...'
-mv ./node_modules/* ../node_modules/
-
-ls ../node_modules
+cp ./node_modules/* ../node_modules/
